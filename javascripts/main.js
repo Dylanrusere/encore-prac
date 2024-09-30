da7f39b32f3729cf2590a2489f5e7378771b52fa
Dylanrusere <dylaneight01@gmail.com>
2024-09-30 16:50:12 +0200

    initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,32 @@
+// Ham Menu
+const hamMenu = document.querySelector(".menu");
+const offMenu = document.querySelector(".offScreenMenu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offMenu.classList.toggle("active");
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.count');
+    
+    counters.forEach(counter => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const current = +counter.innerText;
+
+            const increment = target / 200;
+
+            if (current < target) {
+                counter.innerText = `${Math.ceil(current + increment)}`;
+                setTimeout(updateCounter, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+});
